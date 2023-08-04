5d5996202518ea04de3f28d559ad6081f3d71477
LiamRabbittGitHub <127117919+LiamRabbittGitHub@users.noreply.github.com>
2023-08-04 11:19:23 +0100

    Update desktop_test.go
    added comments that explain what each part does

--- config/desktop_test.go
+++ config/desktop_test.go
@@ -6,10 +6,13 @@ import (
 	"testing"
 )
 
+// Test function for the GetDesktopPath function
 func TestGetDesktopPath(t *testing.T) {
 	folderInfo, err := os.Stat(GetDesktopPath("standard"))
+// If the directory does not exist, an error is reported in the testing framework
 	if os.IsNotExist(err) {
 		t.Errorf("Failed, expecting directory to exist: %v", err)
 	}
+// Logs the folder information to the console
 	log.Println(folderInfo)
 }
